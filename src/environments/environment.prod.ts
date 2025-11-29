e7d251bb318fee400a86db2c8a185f0644be6d4c
Preethi s <preethis@MacBook-Air.local>
2025-11-29 19:46:04 +0530

    URL updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // Replace with your Render backend URL after deployment
-  apiUrl: 'https://lease-management-application-2.onrender.com'
+  apiUrl: 'https://lease-management-application-3.onrender.com'
 };
