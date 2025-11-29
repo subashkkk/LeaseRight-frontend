049fc16254e3c47311fcfd6f8211d5628e72dd34
Boopathi V <boopathi@gmail.com>
2025-11-29 19:33:22 +0530

    update backend url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // Replace with your Render backend URL after deployment
-  apiUrl: 'https://lease-management-application.onrender.com'
+  apiUrl: ' https://lease-management-application-1-5okk.onrender.com'
 };
